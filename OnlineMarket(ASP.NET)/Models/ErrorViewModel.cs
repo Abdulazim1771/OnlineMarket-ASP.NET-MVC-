efd0bcc5266bb8ac937998560ab70a50e68b4181
Abdulazim1771 <159573274+Abdulazim1771@users.noreply.github.com>
2024-08-25 00:03:50 +0500

    Add files via upload

---
+++ OnlineMarket(ASP.NET)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineMarket_ASP.NET_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
