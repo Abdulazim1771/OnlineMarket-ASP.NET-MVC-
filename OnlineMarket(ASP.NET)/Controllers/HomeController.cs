b3eb20dbeb652c255daa3459a76f9f865d477a49
Abdulazim1771 <159573274+Abdulazim1771@users.noreply.github.com>
2024-10-25 14:42:46 +0500

    Categories binded with dropdown list

--- OnlineMarket(ASP.NET)/Controllers/HomeController.cs
+++ OnlineMarket(ASP.NET)/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using OnlineMarket.Application.Requests.Category;
+using OnlineMarket.Application.Stores.Interfaces;
 using OnlineMarket_ASP.NET_.Models;
 using System.Diagnostics;
 
@@ -7,14 +9,19 @@ namespace OnlineMarket_ASP.NET_.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryStore _categoryStore;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICategoryStore categoryStore)
         {
+            _categoryStore = categoryStore;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index([FromQuery] GetCategoriesRequest getCategoryRequest)
         {
+            var categories = _categoryStore.GetAll(getCategoryRequest);
+            ViewBag.Categories = categories;
+
             return View();
         }
 
