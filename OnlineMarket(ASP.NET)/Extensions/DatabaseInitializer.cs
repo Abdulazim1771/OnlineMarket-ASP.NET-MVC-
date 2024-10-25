9dbf760953a9f40aad50841cc3edabd15b9d7ec1
Abdulazim1771 <159573274+Abdulazim1771@users.noreply.github.com>
2024-10-25 14:42:52 +0500

    Create DatabaseInitializer.cs

---
+++ OnlineMarket(ASP.NET)/Extensions/DatabaseInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Identity;
+using OnlineMarket.Domain.Entities;
+using OnlineMarket.Infrastructure.Persistence;
+using Syncfusion.EJ2.Grids;
+
+namespace OnlineMarket_ASP.NET_.Extensions;
+
+public static class DatabaseInitializer
+{
+    private readonly static Faker _faker = new();
+
+    public static void UseDatabaseInitializer(this WebApplication app)
+    {
+        try
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<OnlineMarketDbContext>();          
+           
+            CreateCategories(context);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unable to seed database. {ex.Message}");
+        }
+    }
+
+    private static void CreateCategories(OnlineMarketDbContext context)
+    {
+        if (context.Categories.Any()) return;
+
+        for (int i = 0; i < 20; i++)
+        {       
+            var category = new Category
+            {    
+                Name = _faker.Lorem.Word(),
+                Description = _faker.Lorem.Sentence(),
+            };
+
+            context.Add(category);
+        }
+
+        context.SaveChanges();
+    }
+}
